859a133d0939299fa544474d2b7ce84d7c8f0f2c
Emil Manolov <emo@stream>
2020-04-22 23:57:16 +0300

    Fix App test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders a login form with new user registration option', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const usernameElement = getByText(/username/i);
+  const passwordElement = getByText(/password/i);
+  const registerElement = getByText(/register/i);
+  expect(usernameElement).toBeInTheDocument();
+  expect(passwordElement).toBeInTheDocument();
+  expect(registerElement).toBeInTheDocument();
 });
